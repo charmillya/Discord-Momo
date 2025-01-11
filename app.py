ae47d38f5cef056b99328d58d1602527e61e57b8
Titouan WISSER <titouanwisser@gmail.com>
2025-01-11 03:03:29 +0100

    Inventory

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ intents.message_content = True
 bot = commands.Bot(command_prefix = "momo ", intents=intents) # instanciation de l'objet bot
 
 nikkisID = '1323383627492364319' # identifiant du serveur nikki's
-emoteNikkiWink = "<:nikki_wink:1326685739148116009>"
-emoteNikkiKiss = "<:nikki_kiss:1326589108361101405>"
-emoteMiraLevel = "<:mira_level:1327334099874087043>"
-emoteMiraExp = "<:mira_exp:1327333514831728720>"
+emotes = {"emoteNikkiWink": "<:nikki_wink:1326685739148116009>",
+           "emoteNikkiKiss": "<:nikki_kiss:1326589108361101405>", 
+           "emoteMiraLevel": "<:mira_level:1327334099874087043>", 
+           "emoteMiraExp": "<:mira_exp:1327333514831728720>", 
+           "emotePendants": "<:pendants:1327454772592246794>", 
+           "emoteWardrobe": "<:wardrobe:1327454758977536123>"}
 
 # levelling system
 
@@ -39,7 +41,7 @@ async def on_message(message):
     if new_xp == neededXp: #this is where you set the threshold for leveling up to the first level
         new_level = old_level+1
         new_xp = 0
-        await message.channel.send(f":up: Congrats {message.author.mention}, you gained a **Mira Level** {emoteMiraLevel} and you are now Level **{new_level}**!")
+        await message.channel.send(f":up: Congrats {message.author.mention}, you gained a **Mira Level** {emotes['emoteMiraLevel']} and you are now Level **{new_level}**!")
     else:
         new_level = old_level
     ###add more logic here for successive level-ups
@@ -81,16 +83,16 @@ async def miralevel(
     if(type == 1):
         miralevelEmbed = nextcord.Embed()
         miralevelEmbed.colour = nextcord.colour.Color.from_rgb(153, 139, 46)
-        miralevelEmbed.title = (f"Stylist {user.name}'s Mira Level {emoteMiraLevel}")
+        miralevelEmbed.title = (f"Stylist {user.name}'s Mira Level {emotes['emoteMiraLevel']}")
         miralevelEmbed.set_thumbnail("https://static.wikia.nocookie.net/infinity-nikki/images/0/07/Mira_Level_Icon.png/revision/latest?cb=20241230202652")
-        miralevelEmbed.add_field(name="Mira Level", value=f'{str((results[2]))} {emoteMiraLevel}')
-        miralevelEmbed.add_field(name="Total", value=f"{str(results[1])} EXP {emoteMiraExp}")
-        miralevelEmbed.add_field(name="Level :up: in", value=f"{str((neededXp - results[0]))} EXP {emoteMiraExp}")
+        miralevelEmbed.add_field(name="Mira Level", value=f'{str((results[2]))} {emotes["emoteMiraLevel"]}')
+        miralevelEmbed.add_field(name="Total", value=f"{str(results[1])} EXP {emotes['emoteMiraExp']}")
+        miralevelEmbed.add_field(name="Level :up: in", value=f"{str((neededXp - results[0]))} EXP {emotes['emoteMiraExp']}")
         await inter.response.send_message(embed = miralevelEmbed)
     else:
-        await inter.response.send_message(f"{user.name}'s **Mira Level {emoteMiraLevel} : {results[2]}** - Total : **{results[1]} EXP {emoteMiraExp}** - Level :up: in **{neededXp - results[0]} EXP {emoteMiraExp}**! {emoteNikkiWink}")
+        await inter.response.send_message(f"{user.name}'s **Mira Level {emotes["emoteMiraLevel"]} : {results[2]}** - Total : **{results[1]} EXP {emotes["emoteMiraExp"]}** - Level :up: in **{neededXp - results[0]} EXP {emotes['emoteMiraExp']}**! {emotes["emoteNikkiWink"]}")
 
-# daily outfit
+# outfit management
 
 @bot.slash_command(
     name="daily",
@@ -123,7 +125,8 @@ async def daily(
         conn.close()
         dailyEmbed = nextcord.Embed()
         dailyEmbed.colour = nextcord.colour.Color.from_rgb(153, 139, 46)
-        dailyEmbed.title = (f"Your daily piece of clothing!")
+        dailyEmbed.title = (f"Your daily piece of clothing {emotes["emoteWardrobe"]}")
+        dailyEmbed.description = "The following piece of clothing has been added to your inventory!"
         dailyEmbed.set_thumbnail("https://static.wikia.nocookie.net/infinity-nikki/images/c/c2/Icon_Wardrobe.png/revision/latest?cb=20241222105101")
         dailyEmbed.add_field(name="Outfit", value=f'{str((results[0]))}')
         dailyEmbed.add_field(name="Name", value=f"{str(results[2])}")
@@ -132,18 +135,45 @@ async def daily(
     else:
         await inter.response.send_message("You've already collected all pieces of clothing!")
 
+@bot.slash_command(
+    name="inventory",
+    description="Checks your inventory!",
+)   
+async def inventory(
+    inter: nextcord.Interaction,
+    user: nextcord.Member = nextcord.SlashOption(
+        required=False,
+        name="stylist",
+        description="View the inventory of a specified stylist!"
+    ),
+    ):
+    user = user or inter.user
+    conn = sqlite3.connect('momodb.db')
+    cur = conn.cursor()
+    cur.execute("SELECT clothes.clothname, outfits.outfitname FROM clothes LEFT OUTER JOIN outfits ON (outfits.outfitid = clothes.outfitid) INNER JOIN obtained ON (clothes.clothid = obtained.clothid) WHERE userid = ? ORDER BY outfitname, clothname ASC", (inter.user.id,))
+    results = cur.fetchall()
+    conn.commit()
+    conn.close()
+    inventoryEmbed = nextcord.Embed()
+    inventoryEmbed.colour = nextcord.colour.Color.from_rgb(153, 139, 46)
+    inventoryEmbed.title = (f"{user.name}'s inventory {emotes["emotePendants"]}")
+    inventoryEmbed.set_thumbnail("https://static.wikia.nocookie.net/infinity-nikki/images/b/b7/Icon_Pendants.png/revision/latest?cb=20241222105801")
+    for i in results:
+        inventoryEmbed.add_field(name=f'{str((i[1]))}', value=f'{str((i[0]))}', inline=False)
+    await inter.response.send_message(embed = inventoryEmbed)
+    
 # commands
 
 @bot.command(name="online?")
 async def SendMessage(ctx):
-    await ctx.send(f'Yes I am ! Momo v0.1 {emoteNikkiKiss}')
+    await ctx.send(f'Yes I am ! Momo v0.1 {emotes["emoteNikkiKiss"]}')
 
 @bot.slash_command( # online command
     name="online",
     description="Tests if I'm online!",
 )   
 async def online(inter: nextcord.Interaction) -> None:
-    await inter.response.send_message(f'I am alive! {emoteNikkiKiss}')
+    await inter.response.send_message(f'I am alive! {emotes["emoteNikkiKiss"]}')
 
 @bot.slash_command( # echo command
     name="echo",
@@ -161,7 +191,7 @@ async def bbq(inter: nextcord.Interaction):
     bbqEmbed.colour = nextcord.colour.Color.from_rgb(153, 139, 46)
     bbqEmbed.title = (f"BBQ fed!")
     bbqEmbed.set_thumbnail("https://cdn-icons-png.flaticon.com/512/7601/7601433.png")
-    bbqEmbed.description = f'Thank you soooo much ehehe! *burp* {emoteNikkiWink}'
+    bbqEmbed.description = f'Thank you soooo much ehehe! *burp* {emotes["emoteNikkiWink"]}'
     await inter.response.send_message(embed = bbqEmbed)
 
 # run
